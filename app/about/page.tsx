47168953f4ced5b2fd7bcc3db81261ab9554ace2
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-05-27 12:35:37 -0400

    initial commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,12 @@
 export default function About() {
+
+    const aboutPageSnippet = () => {
+        return <>
+            <p>
+                This page was made as an educational project and experiment by Sean Rogan, software engineering Student.
+            </p>
+        </>
+    }
     return <>
 
     </>
